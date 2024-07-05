045261d22887beb5d00c074ef3aa0035496ee449
despoetiza <assassinscreedres@gmail.com>
2024-07-04 22:59:58 -0300

    Configuração Inicial + Inicialização do Repositório
    1. Configuração do projeto:
    Crie um projeto Spring Boot usando sua IDE favorita ou utilizando o Spring Initializr.
    Adicione as dependências necessárias do Spring Boot, como spring-boot-starter-web e spring-boot-starter-data-jpa e outros do seu interesse.
    
    2. Inicialização do Repositório Git
    Inicie um repositório Git na raiz do seu projeto.
    Adicione um arquivo gitignore para ignorar arquivos e diretórios desnecessários, como arquivos de compilação, arquivos de log, etc.

---
+++ caseJava/src/main/java/prosper_case/caseJava/CaseJavaApplication.java
@@ -0,0 +1,13 @@
+package prosper_case.caseJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseJavaApplication.class, args);
+	}
+
+}
---
+++ caseJava/src/test/java/prosper_case/caseJava/CaseJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package prosper_case.caseJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
